bc473bc767fc38d965a265830351fb2a09843ad2
annelautrou <annelautrou@gmail.com>
2014-09-22 20:52:56 +0200

    Ex002

--- exercices/002/solution.py
+++ exercices/002/solution.py
@@ -5,4 +5,4 @@
 Ceci est un script temporaire.
 """
 
-print("42")
+print(42)
---
+++ exercices/006/solution.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 19:37:08 2014
+
+@author: administrateur
+"""
+
+name = ["Hello world", 42]
+print(name)
