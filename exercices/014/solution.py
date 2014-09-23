4dc66758bc6d37ae2c3aa4af7b912e821a60c90b
annelautrou <annelautrou@gmail.com>
2014-09-23 15:50:32 +0200

    Ex014

---
+++ exercices/014/solution.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 14:32:14 2014
+
+@author: administrateur
+"""
+
+a = 245850922
+b = 78256779
+e = a / b
+c = (1 + 5**0.5)
+d = 2
+f = c / d
+print(e)
+print(f)
