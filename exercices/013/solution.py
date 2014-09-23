6669d10dcd11f21abefd63b4d390ce48dcc08584
annelautrou <annelautrou@gmail.com>
2014-09-23 14:31:23 +0200

    Ex013

---
+++ exercices/013/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 14:20:37 2014
+
+@author: administrateur
+"""
+
+import sys
+if len(sys.argv) >= 2:
+    print(sys.argv[1])
+else:
+    print("usage: python3 solution.py PARAM")
+    
