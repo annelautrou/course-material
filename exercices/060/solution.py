5ea0484f4c3b1c2f134fbe250848296d2cce90af
annelautrou <annelautrou@gmail.com>
2014-09-23 19:56:07 +0200

    Ex060

---
+++ exercices/060/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 18:11:31 2014
+
+@author: administrateur
+"""
+
+alph = "abcdefghijklmnopqrstuvwxyz"
+for c1 in alph:
+    for c2 in alph:
+        print(c1 + c2)
